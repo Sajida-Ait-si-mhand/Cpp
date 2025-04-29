e6e0fac38b6084d27a03bd48e1defcd6bbd5af13
-🎐  ساجدة  🎐- <153914543+Sajida-Ait-si-mhand@users.noreply.github.com>
2025-04-29 00:10:27 +0000

    1%

---
+++ ex03/Weapon.hpp
@@ -0,0 +1,21 @@
+#pragma once 
+
+#include <iostream>
+class Weapon
+{
+    private:
+        std::string Type;
+    public:
+
+        const std::string &getType() const;
+        const setType(const std::string &type);
+        
+        
+};
+
+class HumanA
+{
+    private:
+
+    public:
+};
