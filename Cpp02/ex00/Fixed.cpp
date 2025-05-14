d085cbb412c7be8eca9d058556ded014aea1a41d
soko <sajidaaitsimhand1@gmail.com>
2025-05-14 23:02:29 +0100

    DONNNe

--- Cpp02/ex00/Fixed.cpp
+++ Cpp02/ex00/Fixed.cpp
@@ -2,30 +2,38 @@
 
 Fixed::Fixed()
 { 
+	std::cout << "Default constructor called" << std::endl;
 	this->FixedPoint = 0;
-}
-
-Fixed::~Fixed()
-{
-	std::cout << "Destructor called" << std::endl;
-}
-void Fixed::setRawBits( int const raw )
-{
-	this->FixedPoint = raw;
-}
-
-int Fixed::getRawBits() const
-{
-	return (this->FixedPoint);
 }
 
 Fixed::Fixed(const Fixed &other)
 {
+	std::cout << "Copy constructor called" << std::endl;
 	this->FixedPoint = other.FixedPoint;
 }
 
 Fixed& Fixed::operator=(const Fixed& other)
 {
+	std::cout << "Copy assignment operator called " << std::endl;
 	this->FixedPoint = other.FixedPoint;
 	return *this;
+}
+
+void Fixed::setRawBits( int const raw )
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	this->FixedPoint = raw;
+}
+
+int Fixed::getRawBits() const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->FixedPoint);
+}
+
+
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
 }
--- Cpp02/ex00/main.cpp
+++ Cpp02/ex00/main.cpp
@@ -1,10 +1,11 @@
 #include "Fixed.hpp"
 
-int main()
-{
-	Fixed a;
-	
-	std::cout << "AFIIN " << std::endl;
-	return 0 ;
-}
-
+int main( void ) {
+  Fixed a;
+  Fixed b( a );
+  Fixed c;
+c = b;
+  std::cout << a.getRawBits() << std::endl;
+  std::cout << b.getRawBits() << std::endl;
+  std::cout << c.getRawBits() << std::endl;
+return 0; }
