bba6f0791863d692b25b6907615615b1161195d0
soko <sajidaaitsimhand1@gmail.com>
2025-07-04 12:31:07 +0100

    I lloVVcode

--- Cpp05/catch.cpp
+++ Cpp05/catch.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
 #include <cstring>
 
-const char *ft_fun()
+// const char *ft_fun()
+// {
+// 	return ("helllo");
+// }
+// int main()
+// {
+// 	try {
+// 		if (strcmp(ft_fun(), "helllo") == 0 )
+// 			throw ft_fun();
+// 	}
+// 	catch (const char *errorCode) {
+// 		std::cout << "Error occurred: " << ft_fun();
+// 	}
+// }
+
+int ft_fun()
 {
-	return ("helllo");
+	return (223);
 }
 int main()
 {
+	int c = ft_fun();
 	try {
-		if (strcmp(ft_fun(), "helljlo"))
-			throw "GOOD";
+		if (23 == c)
+			throw ft_fun();
 	}
-	catch (char *errorCode) {
+	catch (int errorCode) {
 		std::cout << "Error occurred: " << ft_fun();
 	}
 }
